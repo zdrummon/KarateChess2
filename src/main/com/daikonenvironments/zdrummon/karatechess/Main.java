04008b8e21bd8f0d67eeff29d222b25059445a17
zdrummon <zdrummon@gmail.com>
2018-04-07 12:27:15 -0700

    Added more layers of configuration, custom config and jar compile dependencies

--- src/main/com/daikonenvironments/zdrummon/karatechess/Main.java
+++ src/main/com/daikonenvironments/zdrummon/karatechess/Main.java
@@ -1,7 +1,10 @@
 package com.daikonenvironments.zdrummon.karatechess;
 
+import java.util.Arrays;
+import org.apache.commons.lang3.StringUtils;
+
 public class Main {
 	public static void main (String[] args) {
-		System.out.println("Hello world");
+		System.out.println(StringUtils.swapCase(Arrays.toString(args)));
 	}
 }
